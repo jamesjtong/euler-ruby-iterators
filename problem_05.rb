c89695280751194221bcfb378e8fe119a202a6f4
jamesjtong <jtong33@gmail.com>
2014-02-01 01:51:42 -0500

    finishes problem 5 without using lcm or any cheating methods

--- problem_05.rb
+++ problem_05.rb
@@ -1,24 +1,44 @@
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
-# (1..20).inject(:lcm)
+#the cheating way=(1..20).inject(:lcm) 
 
-
+require 'pry'
 class EvenlyDivis
+  attr_accessor :smallest_mult_of_primes, :current_num
   def initialize(max)
     @max = max
   end
 
   def is_prime?(num)
-    num_of_divisors = 0
-    (2..num).each do |divisor|
-      num_of_divisors += 1 if num % divisor == 0 
-    end
-    num_of_divisors == 1 
+    (2..(num-1)).each {|div| num % div == 0? (return false): true}
   end
 
   def find_smallest_num
     array_of_nums = (1..@max).select {|num| is_prime?(num) }
-    array_of_nums.inject {|sum,num| sum * num}
+    @smallest_mult_of_primes=array_of_nums.inject {|sum,num| sum * num}
+    @current_num = @smallest_mult_of_primes
+    increase_until_true
+  end
+
+  def check_true
+    (2..20).all? do |num| 
+      current_num % num == 0 
+    end
+  end
+
+  def increase_until_true
+    until check_true
+      @current_num+=@smallest_mult_of_primes
+      check_true
+    end
+    puts current_num
   end
 end
+
+y=EvenlyDivis.new(20).find_smallest_num
+
+
+#lcm
+
+
