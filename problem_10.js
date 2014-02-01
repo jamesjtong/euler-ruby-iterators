3ff96ccffc2954b71a8a247ca2c48984aae768f3
jamesjtong <jtong33@gmail.com>
2014-02-01 09:58:58 -0500

    finish problem 10, come back to later to find better way

--- problem_10.js
+++ problem_10.js
@@ -1,46 +1,9 @@
-// The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+# // The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
-// Find the sum of all the primes below two million.
-
-
-# def check_p(num)
-#   primes_hash = {}
-#   (2..(Math.sqrt(num))).each do |n|
-#     if num % n == 0 && primes_hash[n] == nil
-#       primes_hash[n] ||= 0
-#       primes_hash[n] += 1
-#     end
-#   end
-# end
-# "require 'pry'
-def summation_of_primes(max)
-  array_of_nums = (0...max).to_a
-  array_of_nums[0] = 0
-  array_of_nums[1] = 0
-  #skip index 1 as thats number 1 and don't want to add that in later, 0 by default wont be added in
-  prod = 0
-  array_of_nums.each do |num|
-    prod = 0
-    next if num == 0
-    while prod ** 2 < max
-      (2..(max)).each do |mult|
-        prod = num * mult
-        array_of_nums[prod] = 0
-      end
-    end
-  end
-
-  p array_of_nums.compact.inject(&:+)
-end
-
-def seive
-
-end
-
-(2...2000000).to_a.map {|num| true}
-
-summation_of_primes(200)
+# // Find the sum of all the primes below two million.
 
 
-# require 'prime'
-# puts Prime.each(1999999).inject(&:+)
+
+
+require 'prime'
+puts Prime.each(1999999).inject(&:+)
